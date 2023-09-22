cc73e8ec1d535b8091054d3fa3d5ef38efa13d62
ricardsanchez <ricardsanchez@es.gestamp.com>
2023-09-22 12:05:26 +0200

    prueba

--- Jueves.cs
+++ Jueves.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,8 +18,93 @@ namespace Pruebas
            PedirEdadesCorregido();
            calificaciones();
             */
-        }
 
+            /* 
+                  * Pedir dos  números enteros al usuario. Pedir hasta que lo que escriba sea un número. Si no es un número,
+                  * Le diremos: "Dato incorrecto, inserta un número, por favor. Y volvemos a leerle
+                  * 
+                  * 
+                  * 
+                  * Mostrar un menu donde:
+                  * 1) Sumar
+                  * 2) Restar
+                  * 3) dividir (validamos !=0 y pedimos número hasta que no sea cero)
+                  * 4) multiplicar
+                  * 0) Salir
+                  * 
+                  * 
+                  * En cada opción del menú, realizaremos la operación y mostraremos el mensaje: "La suma de 3 y 4 es: 7"
+                  *  * 
+
+                  */
+
+            int numA = 0, numB = 0, opción = 0, resultado = 0;
+            bool esNumero = true;
+            string textoResultado = "";
+
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Introduce el 1ºnúmero");
+                    numA = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Introduce el 2ºnúmero");
+                    numB = Convert.ToInt32(Console.ReadLine());
+
+                    esNumero = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Dato incorrecto, inserta un número, por favor");
+                    esNumero = false;
+                }
+
+            } while (!esNumero);
+
+            do
+            {
+                Console.WriteLine("1) Sumar\n2) Restar\n3) dividir\n4) multiplicar\n0) Salir");
+                opción = Convert.ToInt32(Console.ReadLine());
+
+                switch(opción)
+                {
+                    case 0:
+                        textoResultado = "Muchas gracias por usar la aplicación";
+                        break; 
+                    case 1:
+                        resultado = numA + numB;
+                        textoResultado = "La suma de "+ numA+" y " +numB+" es: "+resultado;
+                        break;
+
+                    case 2:
+                        resultado = numA - numB;
+                        textoResultado = "La resta de " + numA + " y " + numB + " es: " + resultado;
+                        break;
+                    case 3:
+                        try
+                        {
+                            resultado = numA / numB;
+                            Console.WriteLine("La división de " + numA + " y " + numB + " es: " + resultado);
+                        }
+                        catch (DivideByZeroException)
+                        {
+                            textoResultado = "NO SE PUEDE DIVIDIR ENTRE CERO";
+                        }
+                        break;
+                    case 4:
+                        resultado = numA * numB;
+                        textoResultado = "La multiplicación de " + numA + " y " + numB + " es: " + resultado;
+                        break;
+                    default: 
+                        textoResultado = "OPCIÓN DE MENÚ NO VÁLIDA";
+                        break;
+                }
+                Console.WriteLine(textoResultado);
+            } while (opción != 0);
+            
+
+        }
         public static void calificaciones()
         {
             /* pedir nota y comprobar que esta entre 0 y 10 */
